b63e10b51b955818ea32d9711e18b77768f2daa1
Greg Vish <gregoryvish@gmail.com>
2022-06-27 13:31:18 +0300

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,5 @@ ROOT="yalcut"
 VERSION="jammy"
 
 debootstrap --arch=arm64 --foreign $VERSION $ROOT http://ports.ubuntu.com/
-tar -czvf $ROOT.tar.gz setup.sh init.sh run.sh $ROOT
+tar -czvf $ROOT.tar.gz setup.sh run.sh $ROOT
 
